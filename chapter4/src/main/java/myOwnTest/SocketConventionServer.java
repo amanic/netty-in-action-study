168ac110698e6e3fbb1db431bc51dbb85bd338e7
martea <1642485710@qq.com>
2019-01-04 18:29:28 +0800

    未完善只是实现功能，不适用readline，找到某具有可行性方案：https://blog.csdn.net/mxy88888/article/details/71629857

--- chapter4/src/main/java/myOwnTest/SocketConventionServer.java
+++ chapter4/src/main/java/myOwnTest/SocketConventionServer.java
@@ -2,6 +2,7 @@ package myOwnTest;
 
 import java.io.BufferedReader;
 import java.io.IOException;
+import java.io.InputStream;
 import java.io.InputStreamReader;
 import java.net.ServerSocket;
 import java.net.Socket;
@@ -15,23 +16,24 @@ public class SocketConventionServer {
     public static void doService() throws IOException {
         int port = 55533;
         final ServerSocket server = new ServerSocket(port);
+        System.out.println("server将一直等待连接的到来");
+        final Socket socket = server.accept();
+        final InputStream inputStream = socket.getInputStream();
         new Thread(new Runnable() {
             @Override
             public void run() {
                 while (true){
                     try {
                         // 监听指定的端口
-
                         // server将一直等待连接的到来
-                        System.out.println("server将一直等待连接的到来");
-                        final Socket socket = server.accept();
                         // 建立好连接后，从socket中获取输入流，并建立缓冲区进行读取
-                        BufferedReader read=new BufferedReader(new InputStreamReader(socket.getInputStream(),"UTF-8"));
-                        String line;
+//                        BufferedReader read=new BufferedReader(new InputStreamReader(socket.getInputStream(),"UTF-8"));
+                        byte[] bytes = new byte[1024];
+                        int len;
                         StringBuilder sb = new StringBuilder();
-                        while ((line = read.readLine()) != null && !sb.toString().contains("end")) {
+                        while ((len = inputStream.read(bytes)) != -1 && !sb.toString().contains("end")) {
                             //注意指定编码格式，发送方和接收方一定要统一，建议使用UTF-8
-                            sb.append(line);
+                            sb.append(new String(bytes, 0, len,"UTF-8"));
                         }
                         System.out.println("get message from client: " + sb);
                         if(sb.toString().equals("bye")){
