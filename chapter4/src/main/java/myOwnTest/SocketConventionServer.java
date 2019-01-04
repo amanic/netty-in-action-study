9403e4c9ca0cbce0a8604358ef7b9a26be9d43c6
martea <1642485710@qq.com>
2019-01-04 18:08:11 +0800

    通过约定符号,告知对方已发送完命令。有疑问待解决。

---
+++ chapter4/src/main/java/myOwnTest/SocketConventionServer.java
@@ -0,0 +1,55 @@
+package myOwnTest;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.net.ServerSocket;
+import java.net.Socket;
+
+public class SocketConventionServer {
+
+    public static void main(String[] args) throws Exception {
+        doService();
+    }
+
+    public static void doService() throws IOException {
+        int port = 55533;
+        final ServerSocket server = new ServerSocket(port);
+        new Thread(new Runnable() {
+            @Override
+            public void run() {
+                while (true){
+                    try {
+                        // 监听指定的端口
+
+                        // server将一直等待连接的到来
+                        System.out.println("server将一直等待连接的到来");
+                        final Socket socket = server.accept();
+                        // 建立好连接后，从socket中获取输入流，并建立缓冲区进行读取
+                        BufferedReader read=new BufferedReader(new InputStreamReader(socket.getInputStream(),"UTF-8"));
+                        String line;
+                        StringBuilder sb = new StringBuilder();
+                        while ((line = read.readLine()) != null && !sb.toString().contains("end")) {
+                            //注意指定编码格式，发送方和接收方一定要统一，建议使用UTF-8
+                            sb.append(line);
+                        }
+                        System.out.println("get message from client: " + sb);
+                        if(sb.toString().equals("bye")){
+                            System.out.println("bye!!!");
+                            break;
+                        }
+                    } catch (IOException e) {
+                        System.out.println("出错了："+e);
+                        break;
+                    }
+                }
+                try {
+                    server.close();
+                } catch (IOException e) {
+                    //TODO
+                }
+                System.out.println("程序结束！！！");
+            }
+        }).start();
+    }
+}
--- chapter4/src/main/java/myOwnTest/package-info.java
+++ chapter4/src/main/java/myOwnTest/package-info.java
@@ -1,7 +1,12 @@
 /**
- * @version 1.0.0  {@link myOwnTest.SocketServer} 作为服务器，可以一直保持开启，接收来自于客户端的消息
+ * @version 1.0.0  单向通信
+ *                 {@link myOwnTest.SocketServer} 作为服务器，可以一直保持开启，接收来自于客户端的消息
  *                 {@link myOwnTest.SocketClient} 作为客户端，可以实现连接服务端并且输入数据传递给服务端，第25行的注释很重要。
  *
+ * @version 1.0.1 双向通信
+ *                {@link myOwnTest.SocketChatServer} 作为服务器端，
+ *                {@link myOwnTest.SocketChatClient} 作为客户端
+ *                客户端在发送完消息后，需要服务端进行处理并返回。
  *
  */
 package myOwnTest;
