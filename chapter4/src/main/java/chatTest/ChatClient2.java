e8d3fd8841e7057e56ac4812a65cb4a7f7a7ec26
martea <1642485710@qq.com>
2019-01-13 20:08:38 +0800

    重写一下各种方法，内容以后再说，先吃饭

--- chapter4/src/main/java/chatTest/ChatClient2.java
+++ chapter4/src/main/java/chatTest/ChatClient2.java
@@ -46,6 +46,76 @@ public class ChatClient2 {
                                 protected void channelRead0(ChannelHandlerContext ctx, String msg) throws Exception {
                                     System.out.println(msg);
                                 }
+
+                                @Override
+                                public boolean acceptInboundMessage(Object msg) throws Exception {
+                                    return super.acceptInboundMessage(msg);
+                                }
+
+                                @Override
+                                public void channelRead(ChannelHandlerContext ctx, Object msg) throws Exception {
+                                    super.channelRead(ctx, msg);
+                                }
+
+                                @Override
+                                public void channelRegistered(ChannelHandlerContext ctx) throws Exception {
+                                    super.channelRegistered(ctx);
+                                }
+
+                                @Override
+                                public void channelUnregistered(ChannelHandlerContext ctx) throws Exception {
+                                    super.channelUnregistered(ctx);
+                                }
+
+                                @Override
+                                public void channelActive(ChannelHandlerContext ctx) throws Exception {
+                                    super.channelActive(ctx);
+                                }
+
+                                @Override
+                                public void channelInactive(ChannelHandlerContext ctx) throws Exception {
+                                    super.channelInactive(ctx);
+                                }
+
+                                @Override
+                                public void channelReadComplete(ChannelHandlerContext ctx) throws Exception {
+                                    super.channelReadComplete(ctx);
+                                }
+
+                                @Override
+                                public void userEventTriggered(ChannelHandlerContext ctx, Object evt) throws Exception {
+                                    super.userEventTriggered(ctx, evt);
+                                }
+
+                                @Override
+                                public void channelWritabilityChanged(ChannelHandlerContext ctx) throws Exception {
+                                    super.channelWritabilityChanged(ctx);
+                                }
+
+                                @Override
+                                public void exceptionCaught(ChannelHandlerContext ctx, Throwable cause) throws Exception {
+                                    super.exceptionCaught(ctx, cause);
+                                }
+
+                                @Override
+                                protected void ensureNotSharable() {
+                                    super.ensureNotSharable();
+                                }
+
+                                @Override
+                                public boolean isSharable() {
+                                    return super.isSharable();
+                                }
+
+                                @Override
+                                public void handlerAdded(ChannelHandlerContext ctx) throws Exception {
+                                    super.handlerAdded(ctx);
+                                }
+
+                                @Override
+                                public void handlerRemoved(ChannelHandlerContext ctx) throws Exception {
+                                    super.handlerRemoved(ctx);
+                                }
                             });
                         }
                     });
